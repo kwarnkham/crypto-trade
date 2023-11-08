a1e646a4cf7538add5408aec727d511c1b0cb3f1
Moon <kwarnkham.moon@gmail.com>
2023-11-08 18:59:32 +0630

    list user transactions

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Deposit;
 use App\Models\Transaction;
+use App\Models\Withdraw;
 use Illuminate\Http\Request;
 
 class TransactionController extends Controller
@@ -11,5 +13,28 @@ class TransactionController extends Controller
     {
         $query = Transaction::query()->latest('id');
         return response()->json($query->paginate($request->per_page ?? 10));
+    }
+
+    public function userTransactions(Request $request)
+    {
+        $filters = $request->validate([
+            'from' => ['required', 'date'],
+            'to' => ['required', 'date']
+        ]);
+
+        $deposits = Deposit::query()->where([
+            ['updated_at', '>=', $filters['from']],
+            ['updated_at', '<=', $filters['to']],
+        ])->latest('updated_at')->get();
+
+        $withdraws = Withdraw::query()->where([
+            ['updated_at', '>=', $filters['from']],
+            ['updated_at', '<=', $filters['to']],
+        ])->latest('updated_at')->get();
+
+        return response()->json([
+            'withdraws' => $withdraws,
+            'deposits' => $deposits
+        ]);
     }
 }
