784ae054845ff16545eee9923818b387555e52f3
Moon <kwarnkham.moon@gmail.com>
2023-11-21 13:46:26 +0630

    fix config

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -30,20 +30,20 @@ return [
 
     'connections' => [
 
-    'pusher' => [
-        'driver' => 'pusher',
-        'key' => env('PUSHER_APP_KEY', 'app-key'),
-        'secret' => env('PUSHER_APP_SECRET', 'app-secret'),
-        'app_id' => env('PUSHER_APP_ID', 'app-id'),
-        'options' => [
-            'cluster' => env('PUSHER_APP_CLUSTER'),
-            'host' => env('PUSHER_HOST', '127.0.0.1'),
-            'port' => env('PUSHER_PORT', 6001),
-            'scheme' => env('PUSHER_SCHEME', 'http'),
-            'encrypted' => true,
-            'useTLS' => env('PUSHER_SCHEME') === 'https',
+        'pusher' => [
+            'driver' => 'pusher',
+            'key' => env('PUSHER_APP_KEY', 'app-key'),
+            'secret' => env('PUSHER_APP_SECRET', 'app-secret'),
+            'app_id' => env('PUSHER_APP_ID', 'app-id'),
+            'options' => [
+                'cluster' => 'ap1',
+                'host' => env('PUSHER_HOST', '127.0.0.1'),
+                'port' => env('PUSHER_PORT', 6001),
+                'scheme' => env('PUSHER_SCHEME', 'http'),
+                'encrypted' => true,
+                'useTLS' => env('APP_ENV') == 'production',
+            ],
         ],
-    ],
 
         'ably' => [
             'driver' => 'ably',
